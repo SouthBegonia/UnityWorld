b373872ab4233a4f2009f953101d87a1ae454107
SouthBegonia <1687634916@qq.com>
2019-05-26 20:09:07 +0800

    BreakBricks project

---
+++ BreakBricks/Assets/Scripts/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");  //x轴
+        float v = Input.GetAxis("Vertical");    //y轴
+        //Debug.Log(h);
+        transform.Translate(new Vector3(h, v, 0) * Time.deltaTime * speed);
+        //左右镜头移动速度1 m/s * speed
+        
+
+    }
+}
---
+++ BreakBricks/Assets/Scripts/Short.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Short : MonoBehaviour
+{
+
+    public GameObject bullet;
+    public float speed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello");
+        //子弹的实例化
+        //GameObject.Instantiate(bullet, transform.position, transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左键按下产生子弹
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject b = GameObject.Instantiate(bullet, transform.position, transform.rotation);
+            Rigidbody rgd = b.GetComponent<Rigidbody>();
+            rgd.velocity = transform.forward * speed;
+        }
+    }
+}
