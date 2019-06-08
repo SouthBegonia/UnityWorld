082b715de1f2631c88a2d31305bba6ada5341695
SouthBegonia <1687634916@qq.com>
2019-06-08 19:51:38 +0800

    TankBattle

---
+++ TankBattle/Tank.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    public float MoveSpeed = 7.0f;      //移动速度
+    public float RotateSpeed = 90f;     //旋转速度
+
+    private void Update()
+    {
+        //键位读取
+        if(Input.GetKey(KeyCode.W))
+            transform.Translate(new Vector3(0, 0, 1) * MoveSpeed * Time.deltaTime);
+        if(Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.forward * -MoveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+            transform.Rotate(new Vector3(0, 1, 0) * -RotateSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.up * RotateSpeed * Time.deltaTime);
+    }
+}
---
+++ TankBattle/TankCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//第三人视角模式，挂载于TankCamera
+public class TankCamera : MonoBehaviour
+{ 
+    public GameObject tank;
+
+    private void LateUpdate()
+    {
+        transform.position = tank.transform.position;
+        //不可再进行视角旋转
+        //transform.rotation = tank.transform.rotation;
+    }
+}
