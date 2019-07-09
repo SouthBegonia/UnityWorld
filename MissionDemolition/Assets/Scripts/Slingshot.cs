8698af6c09afcd5ed5b8104d6ddbc221d5a9e1ed
SouthBegonia <1687634916@qq.com>
2019-07-09 22:06:49 +0800

    场景搭建 实现弹弓核心功能

---
+++ MissionDemolition/Assets/Scripts/Slingshot.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slingshot : MonoBehaviour
+{
+    public GameObject prefabProjectile; //弹丸预制体
+    public float velocityMult = 4f;     //弹丸初速度
+    public bool ______________________;
+    public GameObject launchPoint;      //激活状态高光
+    public Vector3 launchPos;           //弹丸发射位置
+    public GameObject projectile;       //实例化的弹丸
+    public bool aimingMode;             //玩家是否在弹弓上单击鼠标左键
+
+    private void Awake()
+    {
+        //初始化激活状态高光
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+
+        //初始化弹丸发射位置为高光坐标
+        launchPos = launchPointTrans.position;
+    }
+
+    //当鼠标进入Collider或GUIElement内才调用
+    private void OnMouseEnter()
+    {
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
+    }
+
+    //当鼠标从Collider或GUIElement内移出才调用
+    private void OnMouseExit()
+    {
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
+    }
+
+    //当鼠标在Collider或GUIElement内按下左键才调用
+    private void OnMouseDown()
+    {
+        //玩家在鼠标光标悬停在弹弓上方按下了鼠标左键
+        aimingMode = true;
+
+        //实例化一个弹丸，设置坐标及刚体信息
+        projectile = Instantiate(prefabProjectile) as GameObject;
+        projectile.transform.position = launchPos;
+        projectile.GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    private void Update()
+    {
+        //若弹弓未处于瞄准模式则返回
+        if (!aimingMode)
+            return;
+
+        //获取光标在二维窗口中的当前坐标
+        Vector3 mousePos2D = Input.mousePosition;
+
+        //将光标位置转换为三维世界坐标
+        mousePos2D.z = -Camera.main.transform.position.z;
+        Vector3 mousePos3D = Camera.main.ScreenToWorldPoint(mousePos2D);
+
+        //计算launchPos 到 mousePos3D 两点之间的坐标差
+        Vector3 mouseDelta = mousePos3D - launchPos;
+
+        //将mouseDelta 坐标差限制在弹弓的SphereCollider范围内
+        float maxMagnitude = this.GetComponent<SphereCollider>().radius;
+        if(mouseDelta.magnitude > maxMagnitude)
+        {
+            mouseDelta.Normalize();
+            mouseDelta *= maxMagnitude;
+        }
+
+        //将弹丸projectile 移动到新的位置
+        Vector3 projPos = launchPos + mouseDelta;
+        projectile.transform.position = projPos;
+        if (Input.GetMouseButtonUp(0))
+        {
+            //如果松开鼠标
+            aimingMode = false;
+            projectile.GetComponent<Rigidbody>().isKinematic = false;
+
+            /*---弹射出弹丸的方法：施加初速度与施加力，效果相同---*/
+            projectile.GetComponent<Rigidbody>().velocity = -mouseDelta * velocityMult;
+            //projectile.GetComponent<Rigidbody>().AddForce(-mouseDelta * 400f);
+            projectile = null;
+        }
+    }
+}
