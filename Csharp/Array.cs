af5aba560185dadfc23c30ca273892a7be6e50e4
SouthBegonia <1687634916@qq.com>
2019-07-25 14:06:42 +0800

    简单数组

---
+++ Csharp/Array.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class ArrayExample
+{
+    public static void Main()
+    {
+        int[] a = new int[10];
+
+        for (int i = 0; i < a.Length; i++)
+            a[i] = i * i;
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            //Console.WriteLine($"a[{i}] = {a[i]}");
+            Console.WriteLine(string.Format("a[{0}] = {1}",i,a[i]));
+        }
+
+        Console.ReadLine();
+    }
+}
