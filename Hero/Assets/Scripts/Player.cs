8c453263bea5542316d7d8ddafa139e9ab33f5a5
SouthBegonia <1687634916@qq.com>
2019-11-06 18:18:15 +0800

    添加 attack_air_4动作 优化 位移实现/状态判定

--- Hero/Assets/Scripts/Player.cs
+++ Hero/Assets/Scripts/Player.cs
@@ -2,61 +2,99 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public struct PlayerState
+{
+    public bool canInput;
+    public bool canTurn;
+    public bool canAttackAgain;
+    public bool canJump;
+    public bool canSlide;
+
+    public bool isAttacking;
+    public bool isOnGround;
+}
+
+//public enum State
+//{
+//    idle,
+//    //onGround,   
+//    onAir,
+//    atkOnGround,
+//    atkOnAir,
+//    sliding,
+//}
+
 public class Player : MonoBehaviour
 {
-    private Animator animator;
+
+    //Player动作状态机
     private int count = 0;
+    private Animator animator;
     private AnimatorStateInfo stateInfo;
 
-    private bool isAttacking;         //是否在攻击状态 
-    public Vector3 attackMoveDelta; //攻击位移
 
-
-    private bool canAttackAgain;    //防止第三段连击被多次按下，进而导致多段位移
-    private bool canInputMove;
-    private bool canJump;
-    private bool canStand;
-
+    //Player父物体
     private Rigidbody2D rigidbody2d;
-    //private bool isJumping;         //是否在跳跃中
-    private bool isOnGround;        //是否在地面上
-    //private bool isStanding;
+    private Vector2 direction;          //Player面朝的方向
 
     private int moveX;
+    private float SpdMul;
 
+    private PlayerState state;
+
+
+    //PlayerBody
     private BoxCollider2D boxCollider;
 
     private void Start()
     {
         animator = GetComponent<Animator>();
-        attackMoveDelta = Vector3.zero;
         rigidbody2d = gameObject.GetComponentInParent<Rigidbody2D>();
-
         boxCollider = GetComponent<BoxCollider2D>();
 
-        //isStanding = false;
+        state.canInput = true;
+        state.canJump = true;
+        state.canSlide = true;
+        state.canTurn = true;
+        state.canAttackAgain = true;
+        state.isOnGround = true;
+        state.isAttacking = false;
 
-        canJump = true;
-        canStand = true;
-        canInputMove = true;
-        isOnGround = true;
         moveX = 0;
+        SpdMul = 1f;
+        direction = new Vector2(rigidbody2d.transform.localScale.x, 0);
     }
 
     private void Update()
     {
         stateInfo = animator.GetCurrentAnimatorStateInfo(0);
-        InputAttack();
-        
-        if (count == 0)
-            isAttacking = false;
 
-        if (!isAttacking)
+        if (state.isOnGround)
         {
-            InputRun();
+            if (state.isAttacking)
+                SpdMul = 0.1f;
+            else
+                SpdMul = 1.0f;
+        }
+        else if (!state.isOnGround)
+        {
+            if (state.isAttacking)
+                SpdMul = 0.1f;
+            else
+                SpdMul = 1.0f;
         }
 
-        if(transform.parent.transform.eulerAngles != Vector3.zero)
+
+        AttackOnAir();
+
+        AttackOnGround();
+
+        //非特殊情况才输入移动
+        if (state.canInput)
+            InputMove();
+
+
+        if (transform.parent.transform.eulerAngles != Vector3.zero)
         {
             transform.parent.transform.eulerAngles = Vector3.zero;
         }
@@ -65,21 +103,14 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
 
-        if (isAttacking)
-        {
-            transform.parent.position += attackMoveDelta;
-            attackMoveDelta.x = Mathf.Lerp(attackMoveDelta.x, 0f, 0.1f);
+        //if (state.isAttacking)
+        //{
+        //    transform.parent.position += attackMoveDelta;
+        //    attackMoveDelta.x = Mathf.Lerp(attackMoveDelta.x, 0f, 0.1f);
 
-            if (Mathf.Abs(attackMoveDelta.x) < 0.001)
-                attackMoveDelta.x = 0f;
-        }
-
-    }
-
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-
+        //    if (Mathf.Abs(attackMoveDelta.x) < 0.001)
+        //        attackMoveDelta.x = 0f;
+        //}
 
     }
 
@@ -87,9 +118,8 @@ public class Player : MonoBehaviour
     {
         if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Background"))
         {
-            isOnGround = true;
+            state.isOnGround = true;
             animator.SetBool("onGround", true);
-            //isJumping = false;
         }
     }
 
@@ -97,57 +127,52 @@ public class Player : MonoBehaviour
     {
         if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Background"))
         {
-            isOnGround = false;
+            state.isOnGround = false;
             animator.SetBool("onGround", false);
-            //isJumping = true;
         }
     }
 
     //移动输入
-    private void InputRun()
+    private void InputMove()
     {
-        //if(SampleDirection ==)
         int InputX = (int)Input.GetAxisRaw("Horizontal");
 
-
-        if (canInputMove)
+        if (state.canTurn)
         {
             moveX = InputX;
         }
         else
             moveX = 0;
-        
 
-        //滑步中不可读取移动
-        if (canInputMove && !isAttacking)
+        //输入在任何时候（地面/空中）都可以移动玩家：
+        transform.parent.position += new Vector3(moveX * SpdMul, 0, 0) * Time.deltaTime;
+
+        //但只有在地面时才进行玩家转向
+        // && !state.isAttacking
+        if (state.canTurn)
         {
-            transform.parent.position += new Vector3(moveX, 0, 0) * Time.deltaTime;
-
+            //transform.parent.position += new Vector3(moveX, 0, 0) * Time.deltaTime;
             animator.SetInteger("Horizontal", moveX);
 
             if (moveX != 0)
             {
                 if (moveX > 0)
                 {
+                    direction.x = 1;
+
                     transform.parent.localScale = Vector3.one;
-                    if (Mathf.Abs(GetAttackMoveDelta()) < 0.04)
-                    {
-                        AddAttackMoveDelta(0.06f);
-                    }
+                    //if (Mathf.Abs(GetAttackMoveDelta()) < 0.04)
+                    //{
+                    //    AddAttackMoveDelta(0.06f);
+                    //}
                 }
                 else if (moveX < 0)
                 {
+                    direction.x = -1;
                     transform.parent.localScale = new Vector3(-1, 1, 1);
-                    if (Mathf.Abs(GetAttackMoveDelta()) < 0.04)
-                    {
-                        AddAttackMoveDelta(0.06f);
-                    }
                 }
             }
-            else
-            {
-                SetAttackAttackMoveDelta(0f);
-            }
+
         }
 
         if (Input.GetKeyDown(KeyCode.Space))
@@ -155,126 +180,103 @@ public class Player : MonoBehaviour
             //滑步
             if (Input.GetKey(KeyCode.S) && (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)))
             {
-                if (isOnGround && canStand)
+                if (state.isOnGround && state.canSlide)
                 {
-                    animator.SetTrigger("stand");
+                    animator.SetTrigger("slide");
                     rigidbody2d.AddForce(new Vector2(moveX * 30f, 0f));
                     rigidbody2d.velocity += new Vector2(moveX * 1.7f, 0f);
-                    canInputMove = false;
-                    //isStanding = true;
-                    canStand = false;
-                    StartCoroutine(StopStanding());
+
+                    state.canTurn = false;
+                    state.canSlide = false;
+                    StartCoroutine(StopSliding());
                 }
 
             }
             else
             {
                 //跳跃
-                if (canJump && isOnGround)
+                if (state.canJump && state.isOnGround)
                 {
                     //障眼法：jump动画时间和滞空时间大致相同，避免复杂状态
-                    //animator.SetTrigger("jump");
                     animator.SetBool("onGround", false);
                     //rigidbody2d.AddForce(Vector2.up);
                     rigidbody2d.AddForce(new Vector2(moveX * 20f, 10f));
                     rigidbody2d.velocity += Vector2.up * 2f;
+                    state.canTurn = true;
+
                 }
             }
 
         }
     }
 
-    //读取攻击
-    private void InputAttack()
-    {
-        //地面攻击判断
-        if ((stateInfo.IsName("attack_1") || stateInfo.IsName("attack_2") || stateInfo.IsName("attack_3")) && stateInfo.normalizedTime > 1f)
-        {
-            //将count重置为0，即Idle状态
-            count = 0;
-            animator.SetInteger("attack", count);
-
-            isAttacking = false;
-            canJump = true;
-        }
-
-        //Debug.Log(stateInfo.);
-        //空中攻击判断
-        if( (stateInfo.IsName("attack_air_1") || stateInfo.IsName("attack_air_2") || stateInfo.IsName("attack_air_3")) && stateInfo.normalizedTime > 1f)
-        {
-            Debug.Log("sss");
-            count = 0;
-            animator.SetInteger("attack", count);
-            isAttacking = false;
-            canJump = true;
-            //canAttackAgain = true;
-
-        }
-        else if((stateInfo.IsName("jump")||stateInfo.IsName("jump_fall")||stateInfo.IsName("attack_air_1")||stateInfo.IsName("attack_air_2")||stateInfo.IsName("attack_air_3")) && isOnGround)
-        {
-            count = 0;
-            animator.SetInteger("attack", count);
-            isAttacking = false;
-            canJump = true;
-            canAttackAgain = true;
-        }
-
-        //if (stateInfo.IsName("attack_air_1") && stateInfo.normalizedTime>1f)
-        //    Debug.Log("lllllllllll");
-
-        //地面攻击
-        if (isOnGround && Input.GetMouseButtonDown(0))
-        {
-            isAttacking = true;
-            canJump = false;
-            AttackOnGround();
-            //SetAttackAttackMoveDelta(0f);
-            //AddAttackMoveDelta(0.3f);
-        }
-
-        //空中攻击
-        if(!isOnGround && (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.V)))
-        {
-            isAttacking = true;
-            canJump = false;
-            AttackOnAir();
-            //Debug.Log("kkk");
-        }
-    }
 
     //空中攻击
     private void AttackOnAir()
     {
-        //攻击阶段一：
-        if (count == 0 && canAttackAgain)
+        //空中攻击判断：1、2结束时
+        if ((stateInfo.IsName("attack_air_1") || stateInfo.IsName("attack_air_2")) && stateInfo.normalizedTime > 1f)
         {
-            count = 4;
+            count = 0;
             animator.SetInteger("attack", count);
-            canAttackAgain = false;
+            state.isAttacking = false;
+            state.canJump = true;
+            state.canAttackAgain = false;
+        }
+        else if ((stateInfo.IsName("jump") || stateInfo.IsName("jump_fall") || stateInfo.IsName("attack_air_1") || stateInfo.IsName("attack_air_2")) && state.isOnGround)
+        {
+            //空中攻击判断：空中几个普通状态时着陆
+            count = 0;
+            animator.SetInteger("attack", count);
+            state.isAttacking = false;
+            state.canJump = true;
 
-            rigidbody2d.velocity = Vector3.up;
-            rigidbody2d.AddForce(Vector3.up * 5f);
+            state.canAttackAgain = true;
+        }
+        else if (stateInfo.IsName("attack_air_3") && state.isOnGround)
+        {
+            //attack_air_3持续进行直至接触地面，自动切换到attack_air_4
+            //对attack_air_4动画延时结束
+            StartCoroutine(Attck_3CD());
+
 
         }
-        else if (stateInfo.IsName("attack_air_1") && count == 4 && stateInfo.normalizedTime>0.4f)
+
+
+        //空中攻击
+        if (!state.isOnGround && (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.V)) && state.canAttackAgain)
         {
-            //攻击阶段二：
-            count = 5;
-            animator.SetInteger("attack", count);
-            rigidbody2d.velocity = Vector3.up *1.5f;
-            rigidbody2d.AddForce(Vector3.up * 10f);
-        }
-        else if (stateInfo.IsName("attack_air_2") && count == 5 && stateInfo.normalizedTime > 0.4f)
-        {
-            //攻击阶段三： attack_2 ---> attack_3
-            count = 6;
-            animator.SetInteger("attack", count);
-            rigidbody2d.velocity = Vector3.down *2f;
-            rigidbody2d.AddForce(Vector3.down * 10f);
-        }
-        else if (count == 6)
-        {
-                canAttackAgain = true;
+            state.isAttacking = true;
+            state.canJump = false;
+
+            //攻击阶段一：
+            if (count == 0)
+            {
+                count = 4;
+                animator.SetInteger("attack", count);
+                SetMoveDelta(Vector2.up, 1.0f, 5.0f);
+            }
+            else if (stateInfo.IsName("attack_air_1") && count == 4 && stateInfo.normalizedTime > 0.4f)
+            {
+                //攻击阶段二：
+                count = 5;
+                animator.SetInteger("attack", count);
+                SetMoveDelta(Vector2.up, 1.6f, 10f);
+            }
+            else if (stateInfo.IsName("attack_air_2") && count == 5 && stateInfo.normalizedTime > 0.4f)
+            {
+                //攻击阶段三： attack_2 ---> attack_3
+                count = 6;
+                animator.SetInteger("attack", count);
+                SetMoveDelta(Vector2.down, 3f, 0f);
+
+                //三阶段攻击时禁止输入
+                state.canInput = false;
+            }
+            else if (count == 6)
+            {
+                //state.canAttackAgain = true;
+            }
         }
     }
 
@@ -282,76 +284,102 @@ public class Player : MonoBehaviour
     //地面攻击
     private void AttackOnGround()
     {
-        //攻击阶段一： idle ---> attack_1
-        if (count == 0)
+        //地面攻击判断：任意动画结束
+        if ((stateInfo.IsName("attack_1") || stateInfo.IsName("attack_2") || stateInfo.IsName("attack_3")) && stateInfo.normalizedTime > 1f)
         {
-            count = 1;
+            count = 0;
             animator.SetInteger("attack", count);
-            canAttackAgain = true;
+
+            state.isAttacking = false;
+            state.canJump = true;
+            state.canInput = true;
         }
-        else if (stateInfo.IsName("attack_1") && count == 1)
+
+
+        //Input地面攻击：
+        if (state.isOnGround && Input.GetMouseButtonDown(0))
         {
-            //攻击阶段二： attack_1 ---> attack_2
-            count = 2;
-            animator.SetInteger("attack", count);
-            AddAttackMoveDelta(0.2f);
-        }
-        else if (stateInfo.IsName("attack_2") && count == 2 && stateInfo.normalizedTime > 0.2f)
-        {
-            //攻击阶段三： attack_2 ---> attack_3
-            count = 3;
-            animator.SetInteger("attack", count);
-            AddAttackMoveDelta(1.5f);
-        }
-        else if (count == 3)
-        {
-            if (canAttackAgain)
+            rigidbody2d.velocity = Vector2.zero;
+            state.isAttacking = true;
+            state.canJump = false;
+            state.canInput = false;
+
+            //攻击阶段一： idle ---> attack_1
+            if (count == 0)
             {
-                AddAttackMoveDelta(1.2f);
-                canAttackAgain = false;
+                count = 1;
+                animator.SetInteger("attack", count);
+                state.canAttackAgain = true;
             }
+            else if (stateInfo.IsName("attack_1") && count == 1)
+            {
+                //攻击阶段二： attack_1 ---> attack_2
+                count = 2;
+                animator.SetInteger("attack", count);
+                AddMoveDelta(direction, 0.5f, 0f);
+            }
+            else if (stateInfo.IsName("attack_2") && count == 2 && stateInfo.normalizedTime > 0.2f)
+            {
+                //攻击阶段三： attack_2 ---> attack_3
+                count = 3;
+                animator.SetInteger("attack", count);
+            }
+            else if (count == 3)
+            {
+                if (state.canAttackAgain)
+                {
+                    //此处不可再上面攻击阶段三写：添加位移是即时的，但应等attack_3播放一定时长再产生位移
+                    AddMoveDelta(direction, 1.7f, 0f);
+                    state.canAttackAgain = false;
+                }
 
+            }
         }
-        else
-        {
-            SetAttackAttackMoveDelta(0f);
-            //canAttackAgain = false;
-        }
-    }
-
-    void GoToNextAttackAction()
-    {
-        animator.SetInteger("attack", count);
-    }
-
-    private float GetAttackMoveDelta()
-    {
-        return attackMoveDelta.x;
-    }
-
-    public void SetAttackAttackMoveDelta(float x)
-    {
-        attackMoveDelta.x = transform.parent.localScale.x * x / 100f;
-    }
-
-    public void AddAttackMoveDelta(float x)
-    {
-        attackMoveDelta.x += transform.parent.localScale.x * x / 100f;
     }
 
 
-    IEnumerator StopStanding()
+    //设置位移函数：仅用在空中
+    private void SetMoveDelta(Vector2 dir, float velMul, float force)
     {
-        yield return new WaitForSeconds(0.6f);
-        canInputMove = true;
-        //isStanding = false;
+        rigidbody2d.velocity = dir * velMul;
+        rigidbody2d.AddForce(dir * force);
+    }
+
+    //添加位移函数：仅用在地面
+    private void AddMoveDelta(Vector2 dir, float velMul, float ForceMul)
+    {
+        //切勿设置velocity：因为物体的vel不一定是在 direction方向（rigidbody内查看），强行设置会造成误差累计，最终可能造成滑移
+        //同理，AddFore也要考虑物体本身方向
+        rigidbody2d.velocity += dir * velMul;
+        rigidbody2d.AddForce(dir * ForceMul);
+    }
+
+
+    IEnumerator StopSliding()
+    {
+        //0.4s为slide的时长
+        yield return new WaitForSeconds(0.4f);
+        state.canTurn = true;
         rigidbody2d.velocity = Vector2.zero;
-        StartCoroutine(StandCD());
+        StartCoroutine(SlideCD());
     }
 
-    IEnumerator StandCD()
+    IEnumerator SlideCD()
     {
         yield return new WaitForSeconds(0.3f);
-        canStand = true;
+        state.canSlide = true;
+    }
+
+    IEnumerator Attck_3CD()
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        //延时后归位attack
+        count = 0;
+        animator.SetInteger("attack", count);
+        state.canInput = true;
+        state.isAttacking = false;
+        state.canJump = true;
+        state.canAttackAgain = true;
     }
 }
