f0760ec7b2db200232161957896d1034fe8045b9
SouthBegonia <1687634916@qq.com>
2020-07-24 22:52:20 +0800

    编写数据配置表

--- FlappyBird/Assets/GameMain/Scripts/UI/Builtin/UIFormId.cs
+++ FlappyBird/Assets/GameMain/Scripts/UI/Builtin/UIFormId.cs
@@ -6,5 +6,25 @@
     public enum UIFormId
     {
         Undefined = 0,
+
+        /// <summary>
+        /// 菜单界面
+        /// </summary>
+        MenuForm = 1,
+
+        /// <summary>
+        /// 设置界面
+        /// </summary>
+        SettingForm = 2,
+
+        /// <summary>
+        /// 结算界面
+        /// </summary>
+        ScoreForm = 3,
+
+        /// <summary>
+        /// 游戏结束界面
+        /// </summary>
+        GameOverForm = 4
     }
 }
